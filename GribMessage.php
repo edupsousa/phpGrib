72efd2aff0e3581b0a267363490258664da94de9
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2013-09-23 16:19:09 -0300

    Criado parser para arquivos GRIB, leitura dos metadados funcional para empacotamento simples.

---
+++ GribMessage.php
@@ -0,0 +1,111 @@
+<?php
+
+class GribBinaryDataSection
+{
+	const SIMPLE_PACKING = 0;
+	const HARMONIC_SIMPLE_PACKING = 1;
+	const COMPLEX_PACKING = 2;
+	
+	public $sectionLength;
+	public $packingFormat;
+	public $originalDataWereInteger;
+	public $hasAdditionalFlags;
+	public $unusedBytesAtEnd;
+	public $binaryScaleFactor;
+	public $referenceValue;
+	public $datumPackingBits;
+	public $harmonicCoefficientRealPart;
+	public $rawBinaryData;
+}
+
+class GribIndicatorSection
+{
+	public $gribIndicator;
+	public $messageLength;
+	public $editionNumber;
+}
+
+class GribProductDefinitionSection
+{
+	public $sectionLength;
+	public $parameterTableVersion;
+	public $centerIdentification;
+	public $generatingProcessId;
+	public $gridIdentification;
+	public $hasGDS;
+	public $hasBMS;
+	public $parameterAndUnits;
+	public $typeOfLayerOrLevel;
+	public $layerOrLevelValue;
+	public $year;
+	public $month;
+	public $day;
+	public $hour;
+	public $minute;
+	public $forecastTimeUnit;
+	public $periodOfTime1;
+	public $periodOfTime2;
+	public $timeRangeIndicator;
+	public $numberIncluded;
+	public $numberMissing;
+	public $century;
+	public $subcenterIdentification;
+	public $decimalScaleFactor;
+	public $reserved;
+}
+
+class GribGridDescriptionSection
+{
+	public $sectionLength;
+	public $numberOfVerticalCoordinateParameters;
+	public $pvOrPl;
+	public $dataRepresentationType;
+	public $gridDescription;
+}
+
+class GribGridDescription
+{
+	
+}
+
+class GribLatLonGridDescription extends GribGridDescription
+{
+	const EARTH_SPHERICAL = 0;
+	const EARTH_SPHEROID = 1;
+	
+	const DIRECTION_NORTH_EAST = 0;
+	const DIRECTION_BY_GRID = 1;
+	
+	public $pointsAlongLatitude;
+	public $pointsAlongLongitude;
+	
+	public $latitudeFirstPoint;
+	public $latitudeFirstPointIsSouth;
+	public $longitudeFirstPoint;
+	public $longitudeFirstPointIsWest;
+	
+	public $directionIncrementGiven;
+	public $earthModel;
+	public $componentsDirection;
+	
+	public $latitudeLastPoint;
+	public $latitudeLastPointIsSouth;
+	public $longitudeLastPoint;
+	public $longitudeLastPointIsWest;
+	
+	public $longitudinalIncrement;
+	public $latitudinalIncrement;
+	
+	public $scanNegativeI;
+	public $scanNegativeJ;
+	public $scanJConsecutive;
+}
+
+class GribMessage
+{
+	public $indicatorSection;
+	public $productDefinitionSection;
+	public $gridDescriptionSection;
+	public $bitmapSection;
+	public $binaryDataSection;
+}
