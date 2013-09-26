2ac5a0d7d412ab62e97e73d58c51d58be3ec11ba
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2013-09-25 23:57:13 -0300

    Documentadas funções da classe GribParser

--- GribParser.php
+++ GribParser.php
@@ -37,6 +37,22 @@ class GribParserException extends Exception
  */
 class GribParser
 {
+	/**
+	 * This function is used to retrieve a GRIB message section as a binary 
+	 * string.
+	 * The section is retrieved from the $message parameter starting from the
+	 * pointer $currentPosition. If the optional $length parameter is not set
+	 * the section length is retrieved from 3 bytes starting at $currentPosition
+	 * as a unsigned integer.
+	 * After retrieving the $currentPosition pointer is updated to 1 position
+	 * after the retrieved section.
+	 * 
+	 * @param string $message A binary string containing the entire GRIB message
+	 * @param integer $currentPosition A pointer to the current position of the 
+	 * message being retrieved
+	 * @param integer $length (Optional) The length of the section to retrieve
+	 * @return string A binary string containing the section data
+	 */
 	protected static function _getRawSectionFromMessage($message, &$currentPosition, $length = false)
 	{
 		if ($length === false)
@@ -47,23 +63,60 @@ class GribParser
 		return $raw;
 	}
 	
+	/**
+	 * Retrieve the length of the next section in the GRIB message.
+	 * The length is retrieved as a unsigned integer from 3 bytes starting
+	 * in the $currentPosition
+	 * 
+	 * @param string $message The binary GRIB message
+	 * @param integer $currentPosition A pointer to the current position of the 
+	 * message
+	 * @return integer The length of the section
+	 */
 	protected static function _getNextSectionLength($message, $currentPosition)
 	{
 		return self::_getUInt($message, $currentPosition, 3);
 	}
 	
+	/**
+	 * Check if a bit flag is set
+	 * 
+	 * @param integer $flag The bit value to check
+	 * @param string $string The string where containing the flag
+	 * @param integer $position The position of the byte containing the flag 
+	 * on the $string
+	 * @return boolean True if the flag is set, False if not
+	 */
 	protected static function _isFlagSet($flag, $string, $position)
 	{
 		$byte = ord(substr($string, $position, 1));
 		return (($byte & $flag) == $flag);
 	}
 	
+	/**
+	 * Retrieve a unsigned integer from the binary string at position of $start
+	 * parameter, the length of the integer is machine independent given by 
+	 * the $length parameter.
+	 * 
+	 * @param string $string The binary string
+	 * @param integer $start Where to find the value
+	 * @param integer $length The byte length of the value
+	 * @return integer The value retrieved from the binary string
+	 */
 	protected static function _getUInt($string, $start, $length)
 	{
 		$value = unpack('H*', substr($string, $start, $length));
 		return hexdec($value[1]);
 	}
 	
+	/**
+	 * Retrieve a signed integer from a binary string.
+	 * 
+	 * @param string $string The binary string
+	 * @param integer $start Position of the first byte
+	 * @param integer $length Byte length of the signed integer
+	 * @return integer Return a signed integer value
+	 */
 	protected static function _getSignedInt($string, $start, $length)
 	{
 		$uInt = self::_getUInt($string, $start, $length);
@@ -72,6 +125,13 @@ class GribParser
 		return ($signal ? -$value : $value);
 	}
 	
+	/**
+	 * Retrieve a single precision float point from a binary string.
+	 * 
+	 * @param string $string The binary string
+	 * @param integer $position The position of the first byte to retrieve
+	 * @return float A single precision float point
+	 */
 	protected static function _getSingle($string, $position)
 	{
 		$A = self::_getSignedInt($string, $position, 1);
