9af53908512e16554417da35a3b896b6c2124231
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2013-09-30 22:34:08 -0300

    Refatorada função getMessages

--- parser/GribFileParser.php
+++ parser/GribFileParser.php
@@ -23,39 +23,54 @@ class GribFileParser extends GribParser
 	 * specified by the path.
 	 * 
 	 * @param string $path The path of the file to retrieve GRIB messages
-	 * @return array A array containing all GRIB messages from the 
+	 * @return GribMessage[] A array containing all GRIB messages from the 
 	 * file as GribMessage objects
 	 * @throws GribParserException
 	 */
-	public static function getMessages($path)
+	public static function loadFile($path)
 	{
 		$handle = fopen($path,'rb');
 		if (!$handle)
 			throw new GribParserException('',  GribParserException::UNABLE_TO_OPEN_FILE);
 		
 		$messages = array();
-		while (self::_fileHasGribMessage($handle)) {
+		while ($messages[] = self::readMessage($handle));
+		array_pop($messages);
+		
+		fclose($handle);
+		return $messages;
+	}
+	
+	/**
+	 * Read a GribMessage from the file at current position.
+	 * 
+	 * @param resource $handle The handle resource created with fopen
+	 * @return GribMessage A representation from the GRIB message read from file
+	 * @throws GribParserException
+	 */
+	public static function readMessage($handle)
+	{
+		if (self::_fileHasGribMessage($handle)) {
 			$messageSize = self::_readMessageSizeFromFile($handle);
 			$gribVersion = ord(self::_readStringFromFile($handle, self::GRIB_VERSION_FIELD_LENGHT));
 			
 			if ($gribVersion != 1)
 				throw new GribParserException('', GribParserException::UNSUPPORTED_GRIB_VERSION);
 
-				/*
-				 * Rewind file pointer to the beginning of Indicator Section 
-				 * to read entire GRIB message for parsing.
-				 */
-				fseek($handle,
-					-(self::MESSAGE_IDENTIFICATOR_LENGHT + 
-					  self::MESSAGE_SIZE_FIELD_LENGHT +
-					  self::GRIB_VERSION_FIELD_LENGHT
-					),
-					SEEK_CUR);
-				$gribMessage = self::_readStringFromFile($handle, $messageSize);
-				$messages[] = GribMessageParser::parse($gribMessage);
+			/*
+			 * Rewind file pointer to the beginning of Indicator Section 
+			 * to read entire GRIB message for parsing.
+			 */
+			fseek($handle,
+				-(self::MESSAGE_IDENTIFICATOR_LENGHT + 
+				  self::MESSAGE_SIZE_FIELD_LENGHT +
+				  self::GRIB_VERSION_FIELD_LENGHT
+				),
+				SEEK_CUR);
+			$gribMessage = self::_readStringFromFile($handle, $messageSize);
+			return GribMessageParser::parse($gribMessage);
 		}
-		fclose($handle);
-		return $messages;
+		return false;
 	}
 	
 	/**
